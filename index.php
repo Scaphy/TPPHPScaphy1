a017d534213ccfc13fbed0405efa2c05b63336ca
scaphydata <scaphydata@scaphydata.com>
2023-06-15 15:46:11 +0200

    tp01 get Scaphy

--- index.php
+++ index.php
@@ -42,3 +42,46 @@ echo "Le resultat est : $resultat";
 }
 
 ?>
+/*
+    Créer un petit programme qui permet de saisir un prix HT
+    et d'afficher le prix TTC correspondant
+    // INFORMATIONS
+    - Le prix HT, doit être renseigné directement sur le formulaire sur la page (get)
+    - Vous allez faire le calcul et stocker le montant TTC sur une variable
+    - Vous allez faire un echo affichant :
+        Le prix HT est = (ajouter la variable du prixHT), le prix TTC est = (ajouter la variable du prix TTC), le montant total de la TVA est = (ajoutez le montant de la TVA)
+    - Taux TVA = 20%
+    // EXEMPLE
+    Si le prix HT est 10, vous devez afficher :
+    Le prix HT est = 10€, le prix TTC est = 12€, le montant total de la TVA est = 2€
+
+
+ Le prix HT doit être récupéré directement sur le formulaire
+*/
+
+
+<?php 
+
+if(isset($_GET["data"]) && !empty($_GET["data"])){
+            echo "<p> Voici la donnée envoyée : {$_GET["data"]} </p>";
+        } else {
+            echo "<p>Veuillez saisir une donnée.</p>";
+        }
+
+?>
+    <form action="#" method="get">
+        <label for="texte">Entrez un texte :</label>
+        <input type="text" id="texte" name="data" placeholder="Entrez un texte">
+
+        <input type="submit" value="Envoyer !">
+    </form>
+<?php
+
+$prixHT = $_GET["data"];
+$TVA = 20;
+$montantTVA = $prixHT * $TVA/100;
+$prixTTC = $prixHT + $montantTVA;
+
+echo "Le prix HT est = $prixHT €, le prix TTC est = $prixTTC €, le montant total de la TVA est = $montantTVA €";
+
+?>
